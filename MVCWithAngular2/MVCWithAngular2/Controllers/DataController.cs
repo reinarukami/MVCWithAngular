2e5f9b942db680f24af21c5255565013298cabc8
reinarukami <robin.sumalde@gmail.com>
2017-09-15 19:10:33 +0800

    Add files via upload

---
+++ MVCWithAngular2/MVCWithAngular2/Controllers/DataController.cs
@@ -0,0 +1,39 @@
+﻿using MVCWithAngular2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWithAngular2.Controllers
+{
+    public class DataController : Controller
+    {
+        // GET: Data
+        public JsonResult GetLastContact()
+        {
+            Contact c = null;
+
+            using(MyDatabaseEntities dc= new MyDatabaseEntities())
+            {
+                c = dc.Contacts.OrderByDescending(a => a.ContactID).Take(1).FirstOrDefault();
+            }
+
+            return new JsonResult { Data = c, JsonRequestBehavior = JsonRequestBehavior.AllowGet};
+        }
+
+        public JsonResult GetGivenContact(GetContactModels Contact)
+        {
+            Contact c = null;
+
+            using (MyDatabaseEntities db_con = new MyDatabaseEntities())
+            {
+                c = db_con.Contacts.Where(a => a.FirstName == Contact.Name).Take(1).FirstOrDefault();
+                //c = dc.Contacts.OrderByDescending(a => a.ContactID).Take(1).FirstOrDefault();
+            }
+
+            return new JsonResult { Data = c, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+    }
+}
